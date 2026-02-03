6a0daefd34aa8f1e64c84a26c90f2f4b76c40ff6
vudaodev <v24dao@gmail.com>
2026-02-03 23:21:29 +0000

    Import and implement google.genai 'types' so that we can implement multiple messages feature
    In the context of the Google Gen AI Python SDK, types is a module that contains the data structures and helper classes required to build structured requests for the Gemini models.
    
    When you write from google.genai import types, you are importing the classes that define how the model expects to receive and return data.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 load_dotenv()
 
 # Global constants
@@ -20,8 +21,10 @@ def main():
     parser.add_argument("user_prompt", type=str, help="The user should enter a prompt that goes to Google Gemini")
     args = parser.parse_args()
 
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents= args.user_prompt
+        model='gemini-2.5-flash', contents= messages
     )
     print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
     print(response.text)        
