627233b2ac46d5e9db11ca2debf903436fe79fe0
vudaodev <v24dao@gmail.com>
2026-02-03 21:55:19 +0000

    Set up project

---
+++ main.py
@@ -0,0 +1,22 @@
+# Library imports
+import os
+from dotenv import load_dotenv
+from google import genai
+load_dotenv()
+
+# Global constants
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key == None:
+    raise RuntimeError('API key needed')
+
+# Create new instance of Gemini client
+client = genai.Client(api_key=api_key)
+
+def main():
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print(response.text)        
+
+if __name__ == "__main__":
+    main()
